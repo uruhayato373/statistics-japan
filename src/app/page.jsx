3f4cacc4140a78d33353aaa6dee69f9915ce514e
dicechick373 <dicechick373@gmail.com>
2024-08-20 06:16:25 +0900

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,31 @@
 import SimpleLayout from 'layout/SimpleLayout'
 import Landing from 'views/landing'
 
+// メタデータ生成関数
+export function generateMetadata() {
+  return {
+    title: `統計で見る都道府県`,
+    description,
+    authors: [{ name: 'uruhayato373', url: 'https://github.com/uruhayato373' }],
+    metadataBase: new URL(url),
+    alternates: {
+      canonical: '/',
+    },
+    openGraph: {
+      title: `統計で見る都道府県`,
+      type: 'website',
+      url,
+      images: [
+        {
+          url: 'https://statistics-japan.com/ogp/opengraph-image.png',
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+  }
+}
+
 export default function HomePage() {
   return (
     <SimpleLayout>
