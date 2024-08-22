dafb26e6b14ae87587d2d90fc4c1011455a6960d
uruhayato373 <uruhayato373@gmail.com>
2024-08-22 10:41:46 +0900

    test

--- src/utils/props/metaProps.ts
+++ src/utils/props/metaProps.ts
@@ -37,34 +37,22 @@ const generateMetaProps = async ({
 
   let description = ''
   let url = 'https://statistics-japan.com/'
+  let ogImageUrl = `https://statistics-japan.com/ogp/${fieldId}/${menuId}`
   switch (kindId) {
     case 'japan':
       url += `${fieldId}/${menuId}/japan`
       description += `日本の${currentMenu.menuTitle}をダッシュボード表示。様々な統計値をビジュアライズしています。データはCSV形式でダウンロードも可能。`
+      ogImageUrl += '/japan/00000.png'
       break
     case 'prefecture-rank':
       url += `${fieldId}/${menuId}/prefecture-rank/${pageId}`
       description += `都道府県の${currentPage.pageTitle}をコロプレス地図でランキング表示。古い年度のデータも参照できます。データは全てCSV形式でダウンロードも可能。`
+      ogImageUrl += `/prefecture-rank/${pageId}.png`
       break
     case 'prefecture':
       url += `${fieldId}/${menuId}/prefecture/${prefCode}`
       description += `${currentPrefecture.prefName}の${currentMenu.menuTitle}をダッシュボード表示。様々な統計値をビジュアライズしています。データはCSV形式でダウンロードも可能。`
-      break
-  }
-
-  let ogImageUrl = `https://statistics-japan.com/ogp/${fieldId}/${menuId}`
-  switch (kindId) {
-    case 'japan':
-      ogImageUrl += '/japan/00000.png'
-      break
-    case 'prefecture-rank':
-      ogImageUrl += `/prefecture-rank/${pageId}.png`
-      break
-    case 'prefecture':
       ogImageUrl += `/prefecture/${prefCode}.png`
-      break
-    case 'city':
-      ogImageUrl += `/city/${prefCode}/${cityCode}.png`
       break
   }
 
@@ -78,6 +66,7 @@ const generateMetaProps = async ({
     },
     openGraph: {
       title: `${title} | 統計で見る都道府県`,
+      description,
       type: 'website',
       url,
       images: [
@@ -85,8 +74,15 @@ const generateMetaProps = async ({
           url: ogImageUrl,
           width: 1200,
           height: 630,
+          alt: `${title} | 統計で見る都道府県`,
         },
       ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${title} | 統計で見る都道府県`,
+      description,
+      images: [ogImageUrl],
     },
   }
 }
