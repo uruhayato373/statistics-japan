24623f2494fd605424a61276e7c9bd6245f4ed27
uruhayato373 <uruhayato373@gmail.com>
2024-08-22 11:29:00 +0900

    test

--- src/components/apexcharts/ApexPyramidChart.tsx
+++ src/components/apexcharts/ApexPyramidChart.tsx
@@ -69,8 +69,6 @@ export default function ApexPyramidChart({ options, height = 300 }: Props) {
     }
   }, [options, height])
 
-  console.log(customOptions)
-
   return (
     <ReactApexChart
       options={customOptions}
