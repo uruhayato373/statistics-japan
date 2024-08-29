6458fd2617003de273095d7ededec6c74dcf4217
uruhayato373 <uruhayato373@gmail.com>
2024-08-29 11:50:06 +0900

    test

---
+++ src/sections/miningindustry/products/prefecture-rank/MapProductShipmentAmount.tsx
@@ -0,0 +1,62 @@
+import { Suspense } from 'react'
+
+import CircularProgressCards from 'components/CircularProgressCards'
+
+import CardsHighchartsMapChart from 'cards/CardsHighchartsMapChart'
+
+import { saveDocument } from 'app/actions/saveDocument'
+import { saveValues } from 'app/actions/saveValues'
+import handleDocument from 'utils/document'
+import handleEstatAPI from 'utils/e-stat'
+import handleGeoshape from 'utils/geoshape'
+import { RouterProps } from 'utils/props'
+
+const CARD_TITLE = '製造品出荷額'
+const CARD_ID = 'MapProductShipmentAmount'
+
+const ESTAT_PARAMS = {
+  statsDataId: '0000010103',
+  cdCat01: 'C3401',
+}
+
+interface Props {
+  routerProps: RouterProps
+}
+
+// valuesの取得と整形
+async function fetchValues() {
+  const values = await handleEstatAPI().fetchValues({
+    ...ESTAT_PARAMS,
+  })
+
+  return values
+}
+
+// コンポーネントの描画
+export default async function MapProductShipmentAmount({ routerProps }: Props) {
+  const title = `都道府県の${CARD_TITLE}`
+
+  const saveProps = { ...routerProps, cardId: CARD_ID }
+
+  const values = await fetchValues()
+  if (process.env.NODE_ENV === 'development') {
+    await saveValues(saveProps, values)
+  }
+
+  const topojson = await handleGeoshape('prefecture').readJson()
+
+  const document = handleDocument().formatDocument(values)
+  if (process.env.NODE_ENV === 'development') {
+    await saveDocument(saveProps, document)
+  }
+
+  return (
+    <Suspense fallback={<CircularProgressCards />}>
+      <CardsHighchartsMapChart
+        title={title}
+        document={document}
+        topojson={topojson}
+      />
+    </Suspense>
+  )
+}
