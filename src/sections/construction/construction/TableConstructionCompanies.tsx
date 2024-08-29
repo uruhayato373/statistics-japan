f65618ff2d61cd857b01250b2efd7f5a22d138e7
uruhayato373 <uruhayato373@gmail.com>
2024-08-29 18:01:00 +0900

    test

---
+++ src/sections/construction/construction/TableConstructionCompanies.tsx
@@ -0,0 +1,55 @@
+import CardsReactTimeTable from 'cards/CardsReactTimeTable'
+
+import { saveDocument } from 'app/actions/saveDocument'
+import { saveValues } from 'app/actions/saveValues'
+import handleDocument from 'utils/document'
+import handleEstatAPI from 'utils/e-stat'
+import { PrefectureType } from 'utils/prefecture'
+import { RouterProps } from 'utils/props'
+
+const CARD_TITLE = '建設業者数'
+const CARD_ID = 'TableConstructionCompanies'
+
+const ESTAT_PARAMS = {
+  statsDataId: '0000010103',
+  cdCat01: 'C3306',
+}
+
+interface Props {
+  routerProps: RouterProps
+  prefecture: PrefectureType
+}
+
+// valuesの取得と整形
+async function fetchValues(prefCode: string) {
+  const values = await handleEstatAPI().fetchValues({
+    ...ESTAT_PARAMS,
+    cdArea: prefCode,
+  })
+
+  return values
+}
+
+// コンポーネントの描画
+export default async function TableConstructionCompanies({
+  routerProps,
+  prefecture,
+}: Props) {
+  const { prefCode, prefName } = prefecture
+
+  const title = `${prefName}の${CARD_TITLE}`
+
+  const saveProps = { ...routerProps, cardId: CARD_ID }
+
+  const values = await fetchValues(prefCode)
+  if (process.env.NODE_ENV === 'development') {
+    await saveValues(saveProps, values)
+  }
+
+  const document = handleDocument().formatDocument(values)
+  if (process.env.NODE_ENV === 'development') {
+    await saveDocument(saveProps, document)
+  }
+
+  return <CardsReactTimeTable title={title} document={document} />
+}
