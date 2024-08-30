4166ca45bc91c875686352a6bdef41840daecb28
uruhayato373 <uruhayato373@gmail.com>
2024-08-30 13:11:55 +0900

    test

---
+++ src/sections/safetyenvironment/traffic-accident/DashboardNumberOfTrafficAccidents.tsx
@@ -0,0 +1,55 @@
+import CardsDashboardSingle from 'cards/CardsDashboard'
+
+import { saveDocument } from 'app/actions/saveDocument'
+import { saveValues } from 'app/actions/saveValues'
+import handleDocument from 'utils/document'
+import handleEstatAPI from 'utils/e-stat'
+import { PrefectureType } from 'utils/prefecture'
+import { RouterProps } from 'utils/props'
+
+const CARD_TITLE = '交通事故件数'
+const CARD_ID = 'DashboardNumberOfTrafficAccidents'
+
+const ESTAT_PARAMS = {
+  statsDataId: '0000010111',
+  cdCat01: 'K3101',
+}
+
+interface Props {
+  routerProps: RouterProps
+  prefecture: PrefectureType
+}
+
+// valuesの取得と整形
+async function fetchValues(prefCode: string) {
+  const values = await handleEstatAPI().fetchValues({
+    ...ESTAT_PARAMS,
+    cdArea: prefCode,
+  })
+
+  return values
+}
+
+// コンポーネントの描画
+export default async function DashboardNumberOfTrafficAccidents({
+  routerProps,
+  prefecture,
+}: Props) {
+  const { prefCode, prefName } = prefecture
+
+  const title = `${prefName}の${CARD_TITLE}`
+
+  const saveProps = { ...routerProps, cardId: CARD_ID }
+
+  const values = await fetchValues(prefCode)
+  if (process.env.NODE_ENV === 'development') {
+    await saveValues(saveProps, values)
+  }
+
+  const document = handleDocument().formatDocument(values)
+  if (process.env.NODE_ENV === 'development') {
+    await saveDocument(saveProps, document)
+  }
+
+  return <CardsDashboardSingle title={title} document={document} />
+}
---
+++ src/sections/safetyenvironment/traffic-accident/prefecture-rank/MapNumberOfTrafficAccidents.tsx
@@ -0,0 +1,64 @@
+import { Suspense } from 'react'
+
+import CircularProgressCards from 'components/CircularProgressCards'
+
+import CardsHighchartsMapChart from 'cards/CardsHighchartsMapChart'
+
+import { saveDocument } from 'app/actions/saveDocument'
+import { saveValues } from 'app/actions/saveValues'
+import handleDocument from 'utils/document'
+import handleEstatAPI from 'utils/e-stat'
+import handleGeoshape from 'utils/geoshape'
+import { RouterProps } from 'utils/props'
+
+const CARD_TITLE = '交通事故件数'
+const CARD_ID = 'MapNumberOfTrafficAccidents'
+
+const ESTAT_PARAMS = {
+  statsDataId: '0000010111',
+  cdCat01: 'K3101',
+}
+
+interface Props {
+  routerProps: RouterProps
+}
+
+// valuesの取得と整形
+async function fetchValues() {
+  const values = await handleEstatAPI().fetchValues({
+    ...ESTAT_PARAMS,
+  })
+
+  return values
+}
+
+// コンポーネントの描画
+export default async function MapNumberOfTrafficAccidents({
+  routerProps,
+}: Props) {
+  const title = `都道府県の${CARD_TITLE}`
+
+  const saveProps = { ...routerProps, cardId: CARD_ID }
+
+  const values = await fetchValues()
+  if (process.env.NODE_ENV === 'development') {
+    await saveValues(saveProps, values)
+  }
+
+  const topojson = await handleGeoshape('prefecture').readJson()
+
+  const document = handleDocument().formatDocument(values)
+  if (process.env.NODE_ENV === 'development') {
+    await saveDocument(saveProps, document)
+  }
+
+  return (
+    <Suspense fallback={<CircularProgressCards />}>
+      <CardsHighchartsMapChart
+        title={title}
+        document={document}
+        topojson={topojson}
+      />
+    </Suspense>
+  )
+}
