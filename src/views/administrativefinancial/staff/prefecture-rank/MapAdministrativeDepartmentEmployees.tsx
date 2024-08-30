51f73d602ead6d27cd9e8c94335fc4dbdaed765d
uruhayato373 <uruhayato373@gmail.com>
2024-08-30 10:40:30 +0900

    test

---
+++ src/views/administrativefinancial/staff/prefecture-rank/MapAdministrativeDepartmentEmployees.tsx
@@ -0,0 +1,38 @@
+import { Suspense } from 'react'
+
+import Grid from '@mui/material/Grid'
+import Box from '@mui/system/Box'
+
+import Breadcrumbs from 'components/breadcrumbs/Breadcrumbs'
+import CircularProgressViews from 'components/progress/CircularProgressViews'
+
+import MapAdministrativeDepartmentEmployees from 'sections/administrativefinancial/staff/prefecture-rank/MapAdministrativeDepartmentEmployees'
+import handleProps, { RouterProps } from 'utils/props'
+import Error500 from 'views/maintenance/500'
+
+interface Props {
+  routerProps: RouterProps
+}
+
+export default async function PrefectureRankView({ routerProps }: Props) {
+  try {
+    const breadcrumbsProps = await handleProps(routerProps).breadcrumbsProps()
+
+    return (
+      <Suspense fallback={<CircularProgressViews />}>
+        <Breadcrumbs custom icon breadcrumbsProps={breadcrumbsProps} />
+        <Box sx={{ mt: 2.5 }}>
+          <Grid container rowSpacing={4.5} columnSpacing={3}>
+            {/* row 1 */}
+            <Grid item xs={12} md={6} lg={7}>
+              <MapAdministrativeDepartmentEmployees routerProps={routerProps} />
+            </Grid>
+          </Grid>
+        </Box>
+      </Suspense>
+    )
+  } catch (error) {
+    console.error('エラーが発生しました:', error)
+    return <Error500 />
+  }
+}
