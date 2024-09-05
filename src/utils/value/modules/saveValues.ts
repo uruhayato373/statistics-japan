d9d50fe04a48c5c2f9a868ff81d80dfc81aa7605
uruhayato373 <uruhayato373@gmail.com>
2024-09-05 09:11:30 +0900

    test

--- src/utils/value/modules/saveValues.ts
+++ src/utils/value/modules/saveValues.ts
@@ -8,6 +8,8 @@ import { generateLocalFileName } from './generateLocalFileName'
 
 export async function saveValues(cardProps: CardProps, values: ValueType[]) {
   const filename = generateLocalFileName(cardProps)
+  const areaCodes = getUniqueAreaCodes(values)
+  console.log(areaCodes)
   try {
     // ディレクトリが存在しない場合は作成し、データを書き込み
     await fs.outputJson(filename, values, { spaces: 2 })
@@ -20,4 +22,12 @@ export async function saveValues(cardProps: CardProps, values: ValueType[]) {
     }
     return { success: false, message: errorMessage }
   }
+}
+
+function getUniqueAreaCodes(values: ValueType[]): string[] {
+  const uniqueAreaCodesSet = new Set<string>(
+    values.map((value) => value.areaCode)
+  )
+
+  return Array.from(uniqueAreaCodesSet)
 }
