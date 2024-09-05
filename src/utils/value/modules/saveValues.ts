e441116a73d912697b71e34a0dc6383b80bf082d
uruhayato373 <uruhayato373@gmail.com>
2024-09-05 20:29:06 +0900

    test

--- src/utils/value/modules/saveValues.ts
+++ src/utils/value/modules/saveValues.ts
@@ -3,12 +3,13 @@ import { CardProps } from 'utils/props'
 import { ValueType } from '../types/value'
 
 import saveLocalJson from './saveLocal'
-import saveSupabaseDB from './saveSupabaseDB'
+// import saveSupabaseDB from './saveSupabaseDB'
 
 export async function saveValues(cardProps: CardProps, values: ValueType[]) {
   if (process.env.NODE_ENV === 'development') {
     return await saveLocalJson(cardProps, values)
   }
+  return null
 
-  return await saveSupabaseDB(cardProps, values)
+  // return await saveSupabaseDB(cardProps, values)
 }
