d2594ee801072ce9a11e117d547f9fca0af5587e
uruhayato373 <uruhayato373@gmail.com>
2024-09-06 09:10:34 +0900

    test

--- src/views/safetyenvironment/disaster-pollution/prefecture.tsx
+++ src/views/safetyenvironment/disaster-pollution/prefecture.tsx
@@ -7,6 +7,7 @@ import Breadcrumbs from 'components/breadcrumbs/Breadcrumbs'
 import CircularProgressViews from 'components/progress/CircularProgressViews'
 
 import DashboardDisasterDamageAmount from 'sections/safetyenvironment/disaster-pollution/DashboardDisasterDamageAmount'
+import TableDisasterDamageAmount from 'sections/safetyenvironment/disaster-pollution/TableDisasterDamageAmount'
 import handleProps, { RouterProps } from 'utils/props'
 import Error500 from 'views/maintenance/500'
 
@@ -27,6 +28,12 @@ export default async function PrefectureView({ routerProps }: Props) {
             {/* row 1 */}
             <Grid item xs={12} sm={6} md={4} lg={3}>
               <DashboardDisasterDamageAmount
+                routerProps={routerProps}
+                prefecture={currentPrefecture}
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+              <TableDisasterDamageAmount
                 routerProps={routerProps}
                 prefecture={currentPrefecture}
               />
