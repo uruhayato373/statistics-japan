3eb5352d8a9560f57b28c5db0e40b81072e2f180
uruhayato373 <uruhayato373@gmail.com>
2024-09-11 17:02:34 +0900

    test

--- src/cards/CardsHighchartsPrefectureRankingChart/SourceCODH.tsx
+++ src/cards/CardsHighchartsPrefectureRankingChart/SourceCODH.tsx
@@ -8,19 +8,21 @@ import Typography from '@mui/material/Typography'
 
 export default function SourceCODH() {
   return (
-    <Box m={2}>
+    <>
       <Divider sx={{ mb: 2 }} />
-      <Typography variant="caption" color="text.secondary">
-        地図は『
-        <Link
-          href="https://geoshape.ex.nii.ac.jp/city/choropleth/jp_pref.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          歴史的行政区域データセットβ版
-        </Link>
-        』（CODH作成）を利用
-      </Typography>
-    </Box>
+      <Box m={2}>
+        <Typography variant="caption" color="text.secondary">
+          地図は『
+          <Link
+            href="https://geoshape.ex.nii.ac.jp/city/choropleth/jp_pref.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            歴史的行政区域データセットβ版
+          </Link>
+          』（CODH作成）を利用
+        </Typography>
+      </Box>
+    </>
   )
 }
