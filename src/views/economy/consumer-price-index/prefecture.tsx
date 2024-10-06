94648bc7818bcfa023632f5310c6a1ab9c25dcb0
dicechick373 <dicechick373@gmail.com>
2024-10-06 18:00:29 +0900

    test

--- src/views/economy/consumer-price-index/prefecture.tsx
+++ src/views/economy/consumer-price-index/prefecture.tsx
@@ -6,12 +6,10 @@ import Grid from '@mui/material/Grid'
 import Breadcrumbs from 'components/breadcrumbs/Breadcrumbs'
 import CircularProgressViews from 'components/progress/CircularProgressViews'
 
-import DashboardConsumerPriceIndex from 'sections/economy/consumer-price-index/DashboardConsumerPriceIndex'
-import DashboardConsumerPriceIndexChangeRate from 'sections/economy/consumer-price-index/DashboardConsumerPriceIndexChangeRate'
-import DashboardConsumerPriceRegionalDifferenceIndex from 'sections/economy/consumer-price-index/DashboardConsumerPriceRegionalDifferenceIndex'
-import DashboardNationalPriceRegionalDifferenceIndex from 'sections/economy/consumer-price-index/DashboardNationalPriceRegionalDifferenceIndex'
-import TableConsumerPriceIndex from 'sections/economy/consumer-price-index/TableConsumerPriceIndex'
-import TableDepositBalance from 'sections/economy/consumer-price-index/TableDepositBalance'
+import DashboardConsumerPriceRegionalDifferenceIndex from 'sections/economy/consumer-price-index/dashboard/DashboardConsumerPriceRegionalDifferenceIndex'
+import DashboardNationalPriceRegionalDifferenceIndex from 'sections/economy/consumer-price-index/dashboard/DashboardNationalPriceRegionalDifferenceIndex'
+import TableConsumerPriceIndex from 'sections/economy/consumer-price-index/table/TableConsumerPriceIndex'
+import TableDepositBalance from 'sections/economy/consumer-price-index/table/TableDepositBalance'
 import handleProps, { RouterProps } from 'utils/props'
 import Error500 from 'views/maintenance/500'
 
@@ -30,14 +28,14 @@ export default async function PrefectureView({ routerProps }: Props) {
         <Box sx={{ mt: 2.5 }}>
           <Grid container rowSpacing={4.5} columnSpacing={3}>
             {/* row 1 */}
-            <Grid item xs={12} sm={6} md={4} lg={3}>
+            {/* <Grid item xs={12} sm={6} md={4} lg={3}>
               <DashboardConsumerPriceIndex prefecture={currentPrefecture} />
             </Grid>
             <Grid item xs={12} sm={6} md={4} lg={3}>
               <DashboardConsumerPriceIndexChangeRate
                 prefecture={currentPrefecture}
               />
-            </Grid>{' '}
+            </Grid>{' '} */}
             <Grid item xs={12} sm={6} md={4} lg={3}>
               <DashboardConsumerPriceRegionalDifferenceIndex
                 prefecture={currentPrefecture}
