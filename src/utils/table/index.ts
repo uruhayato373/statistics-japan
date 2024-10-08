dd89cb0bba783e4841d681f645cfcc2f6b359080
uruhayato373 <uruhayato373@gmail.com>
2024-10-08 19:31:45 +0900

    test

--- src/utils/table/index.ts
+++ src/utils/table/index.ts
@@ -1,14 +1,35 @@
-import { DocumentType } from 'utils/document'
+import { DocumentType, RankingDocumentType } from 'utils/document'
 
 import formatReactRankTable from './formatReactRankTable'
 import formatReactTable from './formatReactTable'
 
 export type * from './formatReactRankTable'
 
-const formatTable = (document: DocumentType) => {
+// 型ガード関数
+function isRankingDocumentType(
+  doc: DocumentType | RankingDocumentType
+): doc is RankingDocumentType {
+  return 'ranking' in doc
+}
+
+const formatTable = <T extends DocumentType | RankingDocumentType>(
+  document: T
+) => {
   return {
-    reactTable: () => formatReactTable(document),
-    reactRankTable: () => formatReactRankTable(document),
+    reactTable: () => {
+      if (isRankingDocumentType(document)) {
+        throw new Error('reactTable cannot be used with RankingDocumentType')
+      }
+      return formatReactTable(document)
+    },
+    reactRankTable: () => {
+      if (!isRankingDocumentType(document)) {
+        throw new Error(
+          'reactRankTable can only be used with RankingDocumentType'
+        )
+      }
+      return formatReactRankTable(document)
+    },
   }
 }
 
