abed9dbb8c9582c21a3db2d92cd68117abcd4f9a
uruhayato373 <uruhayato373@gmail.com>
2024-10-09 12:58:27 +0900

    test

--- src/types/sections.ts
+++ src/types/sections.ts
@@ -1,8 +1,15 @@
+import { ApexOptions } from 'apexcharts'
+import { Options } from 'highcharts'
+
+import { DocumentType, RankingDocumentType } from 'utils/document'
 import { RouterProps } from 'utils/props'
 
 import { CardsPropsType } from './cards'
 
-export interface SectionsPropsType {
+export interface SectionsPropsType<
+  T extends DocumentType | RankingDocumentType = DocumentType,
+  U extends Options | ApexOptions = ApexOptions,
+> {
   routerProps?: RouterProps
-  children: (props: CardsPropsType) => React.ReactNode
+  children: (props: CardsPropsType<T, U>) => React.ReactNode
 }
