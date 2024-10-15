180a1040381cef4aee0434a291bfc02658651289
uruhayato373 <uruhayato373@gmail.com>
2024-10-15 10:33:32 +0900

    test

--- src/layout/DashboardLayout/Drawer/DrawerContent/NavCard.jsx
+++ src/layout/DashboardLayout/Drawer/DrawerContent/NavCard.jsx
@@ -1,14 +1,12 @@
 import Stack from '@mui/material/Stack'
 
-import AdsResponsive from 'components/adsense/AdsResponsive'
+// import AdsResponsive from 'components/adsense/AdsResponsive'
 import MainCard from 'components/MainCard'
 
 export default function NavCard() {
   return (
     <MainCard sx={{ bgcolor: 'grey.50', m: 3 }}>
-      <Stack alignItems="center">
-        <AdsResponsive />
-      </Stack>
+      <Stack alignItems="center">{/* <AdsResponsive /> */}</Stack>
     </MainCard>
   )
 }
