becc9b82d5a657a58edad805ee8c2a5deeeedd7c
uruhayato373 <uruhayato373@gmail.com>
2024-10-22 15:19:13 +0900

    test

---
+++ src/sections/population/marriage/prefecture-rank/RankingNumberOfDivorcesPerTotalPopulation.tsx
@@ -0,0 +1,71 @@
+import SectionsWrapper from 'components/sections/SectionsWrapper'
+
+import { SectionsPropsType } from 'types/sections'
+import handleDocument, { DocumentType } from 'utils/document'
+import handleEstatAPI from 'utils/e-stat'
+import { ValueType } from 'utils/value'
+
+const CARD_TITLE = '離婚件数（総人口一万人当たり）'
+const CARD_ID = 'ranking-number-of-divorces-per-total-population'
+
+const PAGE_ID = 'number-of-divorces-per-total-population'
+
+// 分子 婚姻件数
+const ESTAT_PARAMS_MOLECULE = {
+  statsDataId: '0000010101',
+  cdCat01: 'A9201',
+}
+
+// 分母 総人口
+const ESTAT_PARAMS_DENOMINATOR = {
+  statsDataId: '0000010101',
+  cdCat01: 'A1101',
+}
+
+// values
+async function processValues() {
+  const { fetchDivisionValues } = handleEstatAPI()
+  const values = await fetchDivisionValues(
+    ESTAT_PARAMS_MOLECULE,
+    ESTAT_PARAMS_DENOMINATOR
+  )
+
+  return formatValues(values)
+}
+
+// format values
+function formatValues(values: ValueType[]): ValueType[] {
+  return values.map((d) => {
+    return {
+      ...d,
+      value: Math.round(d.value * 100000) / 10,
+      categoryName: '婚姻件数',
+      unit: '組/万人',
+    }
+  })
+}
+
+// document
+async function processDocument(values: ValueType[]): Promise<DocumentType> {
+  const { formatRankingDocument } = handleDocument(values)
+  const document = formatRankingDocument()
+
+  return document
+}
+
+// コンポーネントの描画
+export default async function RankingNumberOfDivorces({
+  routerProps,
+  children,
+}: SectionsPropsType) {
+  return (
+    <SectionsWrapper
+      routerProps={{ ...routerProps, pageId: PAGE_ID, cardId: CARD_ID }}
+      cardTitle={CARD_TITLE}
+      processValues={processValues}
+      processDocument={processDocument}
+    >
+      {children}
+    </SectionsWrapper>
+  )
+}
--- src/views/population/marriage/prefecture-rank/NumberOfDivorces.tsx
+++ src/views/population/marriage/prefecture-rank/NumberOfDivorces.tsx
@@ -1,8 +1,13 @@
 import ViewsWrapper from 'components/views//ViewsWrapper'
 import PrefectureRankingCards from 'components/views/PrefectureRankingCards'
+import ScatterChartsSection from 'components/views/ScatterISection'
 
 import RankingNumberOfDivorces from 'sections/population/marriage/prefecture-rank/RankingNumberOfDivorces'
+import RankingNumberOfDivorcesPerTotalPopulation from 'sections/population/marriage/prefecture-rank/RankingNumberOfDivorcesPerTotalPopulation'
+import ScatterNumberOfDivorcesTotalPopulation from 'sections/population/marriage/scatter/ScatterNumberOfDivorcesTotalPopulation'
 import { ViewsPropsType } from 'types/views'
+
+const SCATTER_CHARTS = [ScatterNumberOfDivorcesTotalPopulation]
 
 export default async function NumberOfDivorces({
   routerProps,
@@ -12,6 +17,14 @@ export default async function NumberOfDivorces({
       {/* 離婚件数 */}
       <PrefectureRankingCards
         Section={RankingNumberOfDivorces}
+        routerProps={routerProps}
+      />
+      {/* 相関関係 */}
+      <ScatterChartsSection charts={SCATTER_CHARTS} routerProps={routerProps} />
+      {/* 総人口1万人当たり */}
+      <PrefectureRankingCards
+        title="総人口1万人当たり"
+        Section={RankingNumberOfDivorcesPerTotalPopulation}
         routerProps={routerProps}
       />
     </ViewsWrapper>
