7c5b51d6ad5d44e521e27d62ffa2a6a861a41594
uruhayato373 <uruhayato373@gmail.com>
2024-10-23 21:50:37 +0900

    test

--- src/app/(dashboard)/safetyenvironment/crime/japan/page.tsx
+++ src/app/(dashboard)/safetyenvironment/crime/japan/page.tsx
@@ -1,35 +1,14 @@
-import { Metadata } from 'next'
-import dynamic from 'next/dynamic'
-
-import handleProps from 'utils/props'
+import { JapanPage } from 'components/app/JapanPage'
 
 // 定数
-const FIELD_ID = 'safetyenvironment'
-const MENU_ID = 'crime'
-const KIND_ID = 'japan'
-
-// 動的インポート
-const Japan = dynamic(() => import('views/safetyenvironment/crime/japan'), {
-  suspense: true,
-})
-
-// 共通のhandleProps呼び出し
-const getProps = () =>
-  handleProps({
-    fieldId: FIELD_ID,
-    menuId: MENU_ID,
-    kindId: KIND_ID,
-  })
-
-export const generateMetadata = async (): Promise<Metadata> => {
-  const { metaProps } = getProps()
-  return metaProps()
+const PROPS = {
+  fieldId: 'safetyenvironment',
+  menuId: 'crime',
+  kindId: 'japan',
 }
 
-const Page = () => {
-  const { routerProps } = getProps()
+// ページコンポーネント（共通）
+const { PageComponent, generateMetadata } = JapanPage(PROPS)
 
-  return <Japan routerProps={routerProps} />
-}
-
-export default Page
+export { generateMetadata }
+export default PageComponent
